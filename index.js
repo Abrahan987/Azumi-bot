e32e1d33399ef46ba93dc5b3c912f2792fccdd1d
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2025-01-04 09:55:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Kakaroto Bot Ai ✰');
+console.log('\n✰ Iniciando Azumi ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['redBright']
 });
-say(`Developed By • Legendary`, {
+say(`Developed By • Abrahan-m`, {
 font: 'console',
 align: 'center',
 colors: ['blueBright']
