7df9b770d94e5dc7ac45ff46084ea30b73d670dd
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2025-01-10 00:46:40 -0500

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -9,7 +9,7 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 var handler = m => m
 handler.all = async function (m) {
 
-global.getBuffer = async function getBuffer(url, options) {
+unoglobal.getBuffer = async function getBuffer(url, options) {
 try {
 options ? options : {}
 var res = await axios({
@@ -30,7 +30,7 @@ console.log(`Error : ${e}`)
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 global.fotoperfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
-let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
+let api = await axios.get(`https://delirius-apiofc.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
 let userNationalityData = api.data.result
 global.userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
 let user = global.db.data.users[who]
