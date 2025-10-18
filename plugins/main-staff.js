a458ff76279a5ad7d6bc21663c612fe08228d6de
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2025-10-17 22:49:10 -0500

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -6,27 +6,51 @@ let staff = `✨ *EQUIPO DE AYUDANTES*
 👑 *Propietario:*
 
 • 𝙰𝙱𝚁𝙰𝙷𝙰𝙽
-🤴 *Rol:* Propietario
+🤴 *Rol:* Propietario/mantenimiento/editor
 📱 *Número:* wa.me/573237649689
 
+• JONATHANG
+👨‍💻 *Rol:* staff/Mantenimiento 
+📱 *Número:* wa.me/50584887870
 
-await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+• DEV BLACKDARK
+👨‍💻 *Rol:* Staff/Mantenimiento 
+📱 *Número:* wa.me/5491156178758
+
+• IM F'Z - TESIS
+👨‍💻 *Rol:* Staff/mantenimiento 
+📱 *Número:* wa.me/522431268546
+
+• Gabriel-cell
+👨‍💻 *Rol:* Staff/Soporte 
+📱 *Número:* wa.me/51941247696
+
+• Cristian DEV
+👨‍💻 *Rol:* Staff/Soporte 
+📱 *Número:* wa.me/50378666265
+`
+
+// Imagen que se enviará junto al mensaje
+let imagen = 'https://spacny.wuaze.com//uploads/IMG-20251017-WA1543.jpg'
+
+await conn.sendFile(m.chat, imagen, 'staff.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
 externalAdReply: {
 showAdAttribution: true,
-renderLargerThumbnail: false,
+renderLargerThumbnail: true,
 title: `🥷 Developers 👑`,
 body: `✨ Staff Oficial`,
 mediaType: 1,
 sourceUrl: redes,
-thumbnailUrl: icono
+thumbnailUrl: imagen
 }}
 }, { mentions: m.sender })
-m.react(emoji)
 
+m.react(emoji)
 }
+
 handler.help = ['staff']
 handler.command = ['colaboradores', 'staff']
 handler.register = true
